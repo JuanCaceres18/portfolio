554d72aab1c6d2b9bd2fe2de776c5caac7880a93
Juan Pablo <juanpablocaceres8@gmail.com>
2024-11-13 18:08:33 -0300

    Realizando la validación de datos del form en js

--- portfolio/portfolio_juan/static/js/main.js
+++ portfolio/portfolio_juan/static/js/main.js
@@ -26,20 +26,40 @@ const containerContact = sectionContact.querySelector(".global-container-contact
 const formContact = containerContact.querySelector(".form-container");
 const name = formContact.querySelector("#name");
 const email = formContact.querySelector("#email");
-const number = formContact.querySelector("#number");
+const number = formContact.querySelector("#phone");
 const botonSubmit = formContact.querySelector(".btn-submit-contact");
 
 console.log(name);
 
 formContact.addEventListener("submit", (e) =>{
-  e.preventDefault();
+  let formValido = true;
   if (!name.value){
     name.style.borderBottom = "5px solid red";
+    formValido = false;
   }
-  if (!email.value){
+  else{
+    name.style.borderBottom = "5px solid #F2C94C";
+  }
+  if (!email.value || !email.includes("@")){
     email.style.borderBottom = "5px solid red";
+    formValido = false;
+  }
+  else{
+    email.style.borderBottom = "5px solid #F2C94C";
   }
   if (!number.value){
     number.style.borderBottom = "5px solid red";
+    formValido = false;
   }
+  else{
+    number.style.borderBottom = "5px solid #F2C94C";
+  }
+
+  if(!formValido){
+    e.preventDefault();
+  }
+  else{
+    
+  }
+
 })
