18f1809dd0dccf20c156fd6a27a1ea50a6ed5998
JuanCaceres18 <juanpablocaceres8@gmail.com>
2024-12-06 20:14:55 -0300

    Aplicando info leída en MDN sobre models en Django

--- portfolio/portfolio_juan/models.py
+++ portfolio/portfolio_juan/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length = 50, help_text ="Enter username")
     email = models.CharField(max_length = 100)
     password = models.CharField(max_length = 55)
 
+
+record = 
 
